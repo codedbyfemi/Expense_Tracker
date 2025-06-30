74d0729b5f0c7e84d32ebb636f873ae2a352dd50
Femi6002 <oluwafemiolatunji6002@gmail.com>
2025-06-30 22:50:38 +0100

    Expense Tracker

--- lib/core/database_helper.dart
+++ lib/core/database_helper.dart
@@ -39,5 +39,55 @@ class DatabaseHelper {
   }
 
 // TODO: Add CRUD methods for expenses
-// We'll add these methods next
+// CREATE - Insert a new expense
+  Future<int> insertExpense(Expense expense) async {
+    final db = await database;
+    return await db.insert('expenses', expense.toMap());
+  }
+
+// READ - Get all expenses
+  Future<List<Expense>> getExpenses() async {
+    final db = await database;
+    final List<Map<String, dynamic>> maps = await db.query('expenses');
+
+    return List.generate(maps.length, (i) {
+      return Expense.fromMap(maps[i]);
+    });
+  }
+
+// READ - Get expenses by date range
+  Future<List<Expense>> getExpensesByDateRange(DateTime start, DateTime end) async {
+    final db = await database;
+    final List<Map<String, dynamic>> maps = await db.query(
+      'expenses',
+      where: 'date >= ? AND date <= ?',
+      whereArgs: [start.millisecondsSinceEpoch, end.millisecondsSinceEpoch],
+      orderBy: 'date DESC',
+    );
+
+    return List.generate(maps.length, (i) {
+      return Expense.fromMap(maps[i]);
+    });
+  }
+
+// UPDATE - Modify an existing expense
+  Future<int> updateExpense(Expense expense) async {
+    final db = await database;
+    return await db.update(
+      'expenses',
+      expense.toMap(),
+      where: 'id = ?',
+      whereArgs: [expense.id],
+    );
+  }
+
+// DELETE - Remove an expense
+  Future<int> deleteExpense(int id) async {
+    final db = await database;
+    return await db.delete(
+      'expenses',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
 }
